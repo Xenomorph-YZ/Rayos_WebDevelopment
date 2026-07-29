8849585c3987c5c9fb3b921102b42fb5fba65bc1
Xenomorph-YZ <jaceallenrayos@gmail.com>
2026-07-29 16:15:34 +0800

    no equality ung colors

---
+++ Activity/Activity 6/Activity6.js
@@ -0,0 +1,24 @@
+function changeColor(){
+    document.getElementById("color").src="colors/6A0DAD.png";
+}
+function changeColor2(){
+    document.getElementById("color").src="colors/blue.png";
+}
+function changeColor3(){
+    document.getElementById("color").src="colors/cyan.png";
+}
+function changeColor4(){
+    document.getElementById("color").src="colors/gray.png";
+}
+function changeColor5(){
+    document.getElementById("color").src="colors/lime.png";
+}
+function changeColor6(){
+    document.getElementById("color").src="colors/orange.png";
+}
+function changeColor7(){
+    document.getElementById("color").src="colors/red.png";
+}
+function changeColor8(){
+    document.getElementById("color").src="colors/Yellow.png";
+}
