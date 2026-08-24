ee311be23b821595d3f3f1b43ed5635245b7b744
Xenomorph-YZ <jaceallenrayos@gmail.com>
2026-08-24 15:45:03 +0800

    Activity7 :P

---
+++ Activity/Activity 7/Activity7.js
@@ -0,0 +1,35 @@
+function changeColor(){
+    document.getElementById("grayscaleImg").src="boat.png";
+}
+
+function changeColor2(){
+    document.getElementById("grayscaleImg").src="boxes.png";
+}
+
+function changeColor3(){
+    document.getElementById("grayscaleImg").src="fire.png";
+}
+
+function changeColor4(){
+    document.getElementById("grayscaleImg").src="galaxy.png";
+}
+
+function changeColor5(){
+    document.getElementById("grayscaleImg").src="night_sky.png";
+}
+
+function changeColor6(){
+    document.getElementById("grayscaleImg").src="sea.png";
+}
+
+function changeColor7(){
+    document.getElementById("grayscaleImg").src="sailor.png";
+}
+
+function changeColor8(){
+    document.getElementById("grayscaleImg").src="colored.png";
+}
+
+function changeColor9(){
+    document.getElementById("grayscaleImg").src="grayscale.png";
+}
